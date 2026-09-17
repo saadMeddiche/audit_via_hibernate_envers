7297b07e258dee5ab99d341caa09ba7d4667b62d
SaadMeddiche <saadmeddiche2004201@gmail.com>
2026-09-17 10:43:42 +0100

    the beginning :)

---
+++ src/main/java/com/saadMeddiche/audit_via_hibernate_envers/AuditViaHibernateEnversApplication.java
@@ -0,0 +1,13 @@
+package com.saadMeddiche.audit_via_hibernate_envers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditViaHibernateEnversApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditViaHibernateEnversApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saadMeddiche/audit_via_hibernate_envers/AuditViaHibernateEnversApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saadMeddiche.audit_via_hibernate_envers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditViaHibernateEnversApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
